370b0803ec29538ce6fd55ece4fb53035b9cf6e1
i_oda <i_oda@dreamarts.co.jp>
2021-05-15 15:11:44 +0900

    formula

---
+++ CappuccinoLiblary/Util/Formula.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cappuccino.Util
+{
+    public struct Formula
+    {
+        public double Result { get; private set; }
+
+        private Formula(double n) {
+            Result = n;
+        }
+
+        public Formula Add(double n) {
+            Result += n;
+            return this;
+        }
+
+        public Formula Sub(double n) {
+            Result -= n;
+            return this;
+        }
+
+        public Formula Mul(double n) {
+            Result *= n;
+            return this;
+        }
+
+        public Formula Div(double n) {
+            if (n != 0)
+                Result /= n;
+            else
+                throw new DivideByZeroException();
+            return this;
+        }
+
+        public Formula Func(Func<double, double> a) {
+            Result = a(Result);
+            return this;
+        }
+    }
+}
